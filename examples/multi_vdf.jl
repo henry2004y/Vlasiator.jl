1e34410cc80c4af46122d17adfcc96a2872441fa
Hongyang Zhou <hyzhou@umich.edu>
2021-08-24 15:37:58 +0300

    Add VDF extractor demo

--- examples/multi_vdf.jl
+++ examples/multi_vdf.jl
@@ -1,4 +1,4 @@
-
+## Plot velocity space distributions
 xloc = range(7.0, 10.0, step=2.35)
 yloc = range(-0.0, 5.0, step=2.35)
 zloc = 0.0
@@ -14,3 +14,36 @@ end
 
 for a in axs[end,:] a.set_xlabel("vx [km/s]") end
 for a in axs[:  ,1] a.set_ylabel("vy [km/s]") end
+
+## Subtract data of cells with VDF
+
+using JLD2
+
+filename = "test/data/bulk.1d.vlsv"
+
+meta = load(filename)
+
+cells = getcellwithvdf(meta)
+
+ρ = zeros(Float64, size(cells))
+v = zeros(Float64, 3, size(cells)...)
+p = zeros(Float64, size(cells))
+
+vsize = meta.meshes["proton"].vblock_size .* meta.meshes["proton"].vblocks
+vcellids = [Int64[] for _ in cells]
+vcellf = [Float64[] for _ in cells]
+
+for (i, cell) in enumerate(cells)
+   ρ[i] = readvariable(meta, "proton/vg_rho", cell)[1]
+   v[i] = readvariable(meta, "proton/vg_v", cell)[1]
+   p[i] = readvariable(meta, "vg_pressure", cell)[1]
+
+   vcellids[i], vcellf[i] = readvcells(meta, cell)
+end
+
+jldopen("example.jld2", "a+") do file
+   file["file1/density"] = ρ
+   file["file1/velocity"] = v
+   file["file1/pressure"] = p
+   file["file1/VDF"] = vcellf
+end
