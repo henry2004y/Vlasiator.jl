0068132a0ff85c49709e0bc69550c4e1baae152e
Hongyang Zhou <hyzhou@umich.edu>
2021-09-08 20:22:01 +0300

    Optimized axis setup

--- examples/demo_vars_line_extracting.jl
+++ examples/demo_vars_line_extracting.jl
@@ -16,15 +16,38 @@ point2 = [x2, 0, 0] .* Re
 
 cellids, distances, coords = getcellinline(meta, point1, point2)
 
+loc = range(x1, x2, length=length(cellids))
+
 close(meta.fid)
 
 ρmin, ρmax = 0.0, 10.0     # [amu/cc]
-vmin, vmax = -640.0, -20.0 # [km/s]
+vmin, vmax = -640.0, 0.0   # [km/s]
 pmin, pmax = 0.0, 1.82     # [nPa]
 bmin, bmax = -25.0, 60.0   # [nT]
 
-fig, axs = plt.subplots(4, 1, figsize=(10, 15), sharex=true)
+fontsize = 14
 
+fig, axs = plt.subplots(4, 1, figsize=(10, 15), sharex=true, constrained_layout=true)
+
+axs[end].set_xlim(x1, x2)
+
+axs[1].set_ylim(ρmin, ρmax)
+axs[2].set_ylim(vmin, vmax)
+axs[3].set_ylim(pmin, pmax)
+axs[4].set_ylim(bmin, bmax)
+for ax in axs
+   ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+   ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+   ax.grid(true)
+end
+
+axs[end].set_xlabel("x [Re]"; fontsize)
+axs[1].set_ylabel("Density [amu/cc]"; fontsize)
+axs[2].set_ylabel("Velocity [km/s]"; fontsize)
+axs[3].set_ylabel("Pressure [nPa]"; fontsize)
+axs[4].set_ylabel("Magnetic field [nT]"; fontsize)
+
+# Loop over snapshots
 for (i, fname) in enumerate(filenames)
    println("i = $i/$nfile, filename = $fname")
    local meta = load(fname)
@@ -39,55 +62,44 @@ for (i, fname) in enumerate(filenames)
 
    imagnetopause_ = findfirst(<(0.0), bz)
 
-   loc = range(x1, x2, length=length(rho_extract))
+   axs[1].plot(loc, rho_extract ./ 1e6, label="Proton density", color="#1f77b4")
+   vl1 = axs[1].vlines(loc[imagnetopause_], ρmin, ρmax;
+      colors="r", linestyle="dashed", alpha=0.5)
 
-   axs[1].plot(loc, rho_extract ./ 1e6, label="Proton density")
-   axs[1].vlines(loc[imagnetopause_], ρmin, ρmax, colors="r", linestyle="dashed", alpha=0.5)
+   axs[2].plot(loc, v_extract[1,:] ./ 1e3, label="Vx", color="#1f77b4")
+   vl2 = axs[2].vlines(loc[imagnetopause_], vmin, vmax;
+      colors="r", linestyle="dashed", alpha=0.5)
 
-   axs[2].plot(loc, v_extract[1,:] ./ 1e3, label="Vx")
-   axs[2].vlines(loc[imagnetopause_], vmin, vmax, colors="r", linestyle="dashed", alpha=0.5)
+   axs[3].plot(loc, pdyn_extract, label="Dynamic", color="#1f77b4")
+   axs[3].plot(loc, p_extract, label="Thermal", color="#ff7f0e")
+   vl3 = axs[3].vlines(loc[imagnetopause_], pmin, pmax;
+      colors="r", linestyle="dashed", alpha=0.5)
 
-   axs[3].plot(loc, pdyn_extract, label="Dynamic")
-   axs[3].plot(loc, p_extract, label="Thermal")
-   axs[3].vlines(loc[imagnetopause_], pmin, pmax, colors="r", linestyle="dashed", alpha=0.5)
-
-   axs[4].plot(loc, bz, label="Bz")
-   axs[4].hlines(0.0, loc[1], loc[end], colors="k", linestyle="dashed", alpha=0.2)
-   axs[4].vlines(loc[imagnetopause_], bmin, bmax, colors="r", linestyle="dashed", alpha=0.5)
-
-   axs[end].set_xlim(x1, x2)
-
-   axs[1].set_ylim(ρmin, ρmax)
-   axs[2].set_ylim(vmin, vmax)
-   axs[3].set_ylim(pmin, pmax)
-   axs[4].set_ylim(bmin, bmax)
-   for ax in axs
-      ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
-      ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
-      ax.grid()
-   end
-
-   axs[end].set_xlabel("x [Re]", fontsize=14)
-   axs[1].set_ylabel("Density [amu/cc]", fontsize=14)
-   axs[2].set_ylabel("Velocity [km/s]", fontsize=14)
-   axs[3].set_ylabel("Pressure [nPa]", fontsize=14)
-   axs[4].set_ylabel("Magnetic field [nT]", fontsize=14)
+   axs[4].plot(loc, bz, label="Bz", color="#1f77b4")
+   hl4 = axs[4].hlines(0.0, loc[1], loc[end], colors="k", linestyle="dashed", alpha=0.2)
+   vl4 = axs[4].vlines(loc[imagnetopause_], bmin, bmax;
+       colors="r", linestyle="dashed", alpha=0.5)
 
    str_title = @sprintf "t= %4.1fs" meta.time
-   axs[1].set_title(str_title, fontsize=14)
+   axs[1].set_title(str_title; fontsize)
 
-   axs[2].legend(loc="upper right", fontsize=14)
-   axs[3].legend(loc="upper left", fontsize=14)
-   axs[4].legend(loc="upper right", fontsize=14)
-
-   plt.subplots_adjust(wspace=0.1, hspace=0.05)
+   axs[2].legend(;loc="upper right", fontsize)
+   axs[3].legend(;loc="upper left",  fontsize)
+   axs[4].legend(;loc="upper right", fontsize)
 
    savefig("out/"*lpad(i, 4, '0')*".png", bbox_inches="tight")
 
    for ax in axs
-      ax.cla()
+      for line in ax.get_lines()
+         line.remove()
+      end
    end
+   vl1.remove()
+   vl2.remove()
+   vl3.remove()
+   vl4.remove()
+   hl4.remove()
 end
 
 close(fig)
-println("plot finished!")
+println("Finished!")
