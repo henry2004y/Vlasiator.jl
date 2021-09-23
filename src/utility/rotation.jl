96a6aafb1e75724125e54c6e645e097e610095df
Hongyang Zhou <hyzhou@umich.edu>
2021-09-23 17:44:57 +0300

    Make methods type-stable

--- src/utility/rotation.jl
+++ src/utility/rotation.jl
@@ -8,14 +8,14 @@ using StaticArrays
 
 Rotates `tensor` with a rotation matrix that aligns z-axis with `vector`.
 """
-function rotateTensorToVectorZ(T, v)
-   unitz = @SVector [0.0, 0.0, 1.0]
-   vz = v × unitz
-   vz ./= hypot(vz...)
-   angle = acos(v ⋅ unitz / hypot(v...))
+function rotateTensorToVectorZ(tensor::AbstractMatrix{T}, v::AbstractVector{T}) where T
+   unitz = SVector{3, T}(0.0, 0.0, 1.0)
+   vz = v × unitz::SVector{3, T}
+   vz ./= hypot(vz[1], vz[2], vz[3])
+   angle = acos(v ⋅ unitz / hypot(v[1], v[2], v[3]))
    R = getRotationMatrix(vz, angle)
-   # Rotate Tensor
-   R * T * R'
+   # Rotate tensor
+   R * tensor * R'
 end
 
 """
@@ -25,8 +25,8 @@ Creates a rotation matrix that rotates around a unit `vector` by an `angle` in r
 References: https://en.wikipedia.org/wiki/Rodrigues'_rotation_formula
 https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
 """
-function getRotationMatrix(v, θ)
-   cosθ, sinθ = cos(θ), sin(θ)
+function getRotationMatrix(v::AbstractVector, θ)
+   cosθ, sinθ = eltype(v)(cos(θ)), eltype(v)(sin(θ))
    tmp = 1 - cosθ
    m =  @SMatrix [
         cosθ+v[1]^2*tmp         v[1]*v[2]*tmp-v[3]*sinθ v[1]*v[3]*tmp+v[2]*sinθ;
@@ -41,18 +41,17 @@ Obtain a rotation matrix with each column being a unit vector which is parallel 
 perpendicular (`v1,v2`) to the magnetic field `B`. The two perpendicular directions are
 chosen based on the reference vector of z-axis in the Cartesian coordinates.
 """
-function getRotationB(B)
-   # reference vector
-   v0 = @SVector [0.0, 0.0, 1.0]
-   b = hypot(B...)
+function getRotationB(B::AbstractVector{T}) where T
+   b = hypot(B[1], B[2], B[3])
    if B[3] / b ≈ -1.0 # B aligned with reference vector
-      R = @SMatrix [0.0 -1.0 0.0; 1.0 0.0 0.0; 0.0 0.0 1.0]
+      R = SMatrix{3,3,T}([0.0 -1.0 0.0; 1.0 0.0 0.0; 0.0 0.0 1.0])
    elseif B[3] / b ≈ 1.0 # B aligned with reference vector
-      R = I
+      R = SMatrix{3,3,T}([0.0 -1.0 0.0; 1.0 0.0 0.0; 0.0 0.0 1.0])
    else
-      v3 = @SVector [B[1]/b, B[2]/b, B[3]/b] # unit vector along B
-      v1 = v0 × v3
-      v2 = v3 × v1
+      v0 = SVector{3,T}(0.0, 0.0, 1.0) # reference vector
+      v3 = SVector{3,T}(B[1]/b, B[2]/b, B[3]/b) # unit vector along B
+      v1 = v0 × v3::SVector{3, T}
+      v2 = v3 × v1::SVector{3, T}
 
       R = @SMatrix [v1[1] v2[1] v3[1]; v1[2] v2[2] v3[2]; v1[3] v2[3] v3[3]]
    end
@@ -65,7 +64,7 @@ end
 Rotate the tensor `T` with the 3rd direction aligned with `B`.
 See also: [`rotateWithB!`](@ref)
 """
-function rotateWithB(T, B)
+function rotateWithB(T::AbstractMatrix, B::AbstractVector)
    R = getRotationB(B)
    R * T * R'
 end
@@ -76,7 +75,7 @@ end
 Rotate the tensor `T` with the 3rd direction aligned with `B`.
 See also: [`rotateWithB`](@ref)
 """
-function rotateWithB!(T, B)
+function rotateWithB!(T::AbstractMatrix, B::AbstractVector)
    R = getRotationB(B)
    T[:] = R * T * R'
 end
