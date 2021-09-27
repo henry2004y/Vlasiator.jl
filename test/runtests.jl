2b25c9e2873c3d3f0f2f9e4b4e1af68952845e82
Hongyang Zhou <hyzhou@umich.edu>
2021-09-27 23:03:37 +0300

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -143,7 +143,7 @@ end
 
          @test meta["IonInertial"][1] == 8.578087716535188e7
 
-         @test meta["Larmor"][1] == 142415.61236345078
+         @test readvariable(meta, "Larmor", UInt64[1])[1] == 142415.61236345078
 
          @test meta["Gyroperiod"][1] == 21.834297799454554
 
