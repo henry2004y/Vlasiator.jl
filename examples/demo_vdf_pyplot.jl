8b64d1bc6c83c912aaede9123beeb9ce52166435
Hongyang Zhou <hyzhou@umich.edu>
2021-11-15 22:47:19 +0200

    Update vcell plotting

--- examples/demo_vdf_pyplot.jl
+++ examples/demo_vdf_pyplot.jl
@@ -4,29 +4,24 @@
 
 using Vlasiator, PyPlot
 
-file = "bulk.0000001.vlsv"
+file = "bulk.0001347.vlsv"
 
 meta = load(file)
 
 coordinates = [0.0, 0.0, 0.0]
 
-plot_vdf(meta, coordinates; verbose=true)
+vdfslice(meta, coordinates; verbose=true)
 
 # Show the spatial distribution of cells with saved VDF
 cellswithVDF = getcellwithvdf(meta)
-cellID = meta["CellID"]
-cellID = reshape(cellID, meta.ncells[1], meta.ncells[2])
-f_saved_index_ = [findfirst(==(cid), cellID) for cid in cellswithVDF]
+locations = [getcellcoordinates(meta, cid) for cid in cellswithVDF]
 
-x = LinRange(meta.coordmin[1], meta.coordmax[1], meta.ncells[1]) ./ Vlasiator.Re
-y = LinRange(meta.coordmin[2], meta.coordmax[2], meta.ncells[2]) ./ Vlasiator.Re
-
-xcell = zeros(size(cellswithVDF))
+xcell = zeros(size(locations))
 ycell = similar(xcell)
 
-for i in eachindex(f_saved_index_)
-   xcell[i] = x[f_saved_index_[i][1]]
-   ycell[i] = y[f_saved_index_[i][2]]
+for i in eachindex(locations)
+   xcell[i] = locations[i][1] / Re
+   ycell[i] = locations[i][2] / Re
 end
 
 figure()
